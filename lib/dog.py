e57dec32426ad402f3a2bea98cb80a2686a4d76c
Natalie Wanjiru <natalie.wanjiru@student.moringaschool.com>
2023-08-29 10:15:51 +0300

    New commits

--- lib/dog.py
+++ lib/dog.py
@@ -7,3 +7,45 @@ class Dog:
     def __init__(self,name,breed):
         self.name = name
         self.breed = breed
+
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS dogs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                breed TEXT
+            )
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+   
+    @classmethod
+    def drop_table(cls):
+        sql = """ 
+            DROP TABLE IF EXISTS dogs;
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    def save(self):
+        sql = """
+            INSERT INTO dogs (name, breed)
+            VALUES (?, ?)
+        """
+        CURSOR.execute(sql, (self.name, self.breed))
+        self.id = CURSOR.lastrowid  
+
+        CONN.commit()
+
+    @classmethod
+    def create(cls, name, breed):
+        sql = """
+            INSERT INTO dogs (name, breed)
+            VALUES (?, ?)
+        """
+        CURSOR.execute(sql, (name, breed))
+        new_dog_id = CURSOR.lastrowid
+        CONN.commit()
+
+        return cls(new_dog_id, name, breed)
